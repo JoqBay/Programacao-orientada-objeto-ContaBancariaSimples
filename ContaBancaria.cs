2a00517ff62a6b92803d97af5262370e994f2279
Julio-Maciel-Developer <joqbaydigital@gmail.com>
2022-01-25 18:32:00 -0300

    Métodos adicionaodos

--- ContaBancaria.cs
+++ ContaBancaria.cs
@@ -8,5 +8,96 @@ namespace Programacao_orientada_objeto_ContaBancariaSimples
 {
     public class ContaBancaria
     {
+        private static int numeroDaContaIncremental = 1234567890;
+        public string NumeroDaConta { get; }
+        public string NomeDoProprietarioDaConta { get; set; }
+        public decimal SaldoBalanco
+        {
+            get
+            {
+                decimal saldoBalanco = 0;
+                foreach (var item in todasAsTransacoes)
+                {
+                    saldoBalanco += item.Montante;
+                }
+
+                return saldoBalanco;
+            }
+        }
+
+        private List<Transacao> todasAsTransacoes = new List<Transacao>();
+
+
+        private readonly decimal saldoMinimoBalanco;
+
+        public ContaBancaria(string nome, decimal saldoInicialBalanco) : this(nome, saldoInicialBalanco, 0)
+        {
+
+        }
+
+
+        public ContaBancaria(string nome, decimal saldoInicialBalanco, decimal saldoMinimoBalanco)
+        {
+            NumeroDaConta = numeroDaContaIncremental.ToString();
+            numeroDaContaIncremental++;
+
+            NomeDoProprietarioDaConta = nome;
+            this.saldoMinimoBalanco = saldoMinimoBalanco;
+            if (saldoInicialBalanco > 0)
+                FazerDeposito(saldoInicialBalanco, DateTime.Now, "Saldo inicial da conta");
+        }
+
+
+
+        public void FazerDeposito(decimal montante, DateTime data, string descricao)
+        {
+            if (montante <= 0)
+                throw new ArgumentOutOfRangeException(nameof(montante), "O valor de depósito deve ser positivo");
+
+            var deposito = new Transacao(montante, data, descricao);
+            todasAsTransacoes.Add(deposito);
+        }
+
+
+        public void FazerSaque(decimal montante, DateTime data, string descricao)
+        {
+            if (montante <= 0)
+                throw new ArgumentOutOfRangeException(nameof(montante), "O valor de saque deve ser positivo");
+
+            var transacaoDeChequeEspecial = ChecarLimiteDeSaqueParaTerOpcoes(SaldoBalanco - montante < saldoMinimoBalanco);
+            var saque = new Transacao(-montante, data, descricao);
+            todasAsTransacoes.Add(saque);
+            if (transacaoDeChequeEspecial != null)
+                todasAsTransacoes.Add(transacaoDeChequeEspecial);
+        }
+
+
+        protected virtual Transacao? ChecarLimiteDeSaqueParaTerOpcoes(bool issoEOverdrawn)
+        {
+            if (issoEOverdrawn)
+            {
+                throw new InvalidOperationException("Fundos insuficientes para esse saque");
+            }
+            else
+            {
+                return default;
+            }
+        }
+
+
+        public string ObterHistoricoDasTransacoesdaContaBancaria()
+        {
+            var relatorioDastransacoes = new System.Text.StringBuilder();
+
+            decimal saldoBalanco = 0;
+            relatorioDastransacoes.AppendLine($"Data       \b\tMontante      \b\tDescrição da Transação");
+            foreach (var item in todasAsTransacoes)
+            {
+                saldoBalanco += item.Montante;
+                relatorioDastransacoes.AppendLine($"{item.DataDaTransacao.ToShortDateString()}    \b\t{item.Montante}         \b\t{item.DescricaoDaTransacao}");
+            }
+
+            return relatorioDastransacoes.ToString();
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,2 +1,37 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using Programacao_orientada_objeto_ContaBancariaSimples;
+
+
+var conta01 = new ContaBancaria("Julio Maciel", 10000);
+
+Console.WriteLine("Conta bancária nº1");
+Console.WriteLine();
+
+Console.WriteLine($"Número da conta: {conta01.NumeroDaConta}");
+Console.WriteLine();
+Console.WriteLine($"Nome do proprietário da conta: {conta01.NomeDoProprietarioDaConta}");
+Console.WriteLine();
+Console.WriteLine($"Saldo: {conta01.SaldoBalanco}");
+
+Console.WriteLine();
+
+Console.WriteLine("--------------------------");
+Console.WriteLine();
+
+
+var conta02 = new ContaBancaria("Bruna Karoline Moraes Maciel", 50000);
+
+Console.WriteLine("Conta bancária nº2");
+
+Console.WriteLine();
+
+Console.WriteLine($"Número da conta: {conta02.NumeroDaConta}");
+Console.WriteLine();
+Console.WriteLine($"Nome do proprietário da conta: {conta02.NomeDoProprietarioDaConta}");
+Console.WriteLine();
+Console.WriteLine($"Saldo: {conta02.SaldoBalanco}");
+
+// Algumas transações da Conta nº1
+conta01.FazerDeposito(1000, DateTime.Now, "Uma venda");
+Console.WriteLine(conta01.SaldoBalanco);
+Console.WriteLine(conta01.ObterHistoricoDasTransacoesdaContaBancaria());
