e0eef8eb14926d89b71c41696ada5e97e495a3bb
Julio-Maciel-Developer <joqbaydigital@gmail.com>
2022-01-25 15:28:12 -0300

    Classe criada para ajudar no gerenciamento das transações

---
+++ Transacao.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Programacao_orientada_objeto_ContaBancariaSimples
+{
+    public class Transacao
+    {
+        public decimal Montante { get; }
+        public DateTime DataDaTransacao { get; }
+        public string DescricaoDaTransacao { get; set; }
+
+        public Transacao(decimal montante, DateTime data, string descricao)
+        {
+            Montante = montante;
+            DataDaTransacao = data;
+            DescricaoDaTransacao = descricao;
+        }
+    }
+}
