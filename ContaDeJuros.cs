35bdf5d2fe7c6401de0c92da6fcdd470824bec97
Julio-Maciel-Developer <joqbaydigital@gmail.com>
2022-01-27 10:17:33 -0300

    Classe criada para add verba extra

---
+++ ContaDeJuros.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Programacao_orientada_objeto_ContaBancariaSimples
+{
+    public class ContaDeJuros : ContaBancaria
+    {
+        public ContaDeJuros(string nome, decimal saldoInicialBalanco) : base(nome, saldoInicialBalanco)
+        {
+        }
+
+        public override void RealizarTransacaoDeFimDeMes()
+        {
+            if (SaldoBalanco > 500m)
+            {
+                var juros = SaldoBalanco * 0.05m;
+                FazerDeposito(juros, DateTime.Now, "Aplicar juros mensais");
+            }
+            
+
+        }
+    }
+}
