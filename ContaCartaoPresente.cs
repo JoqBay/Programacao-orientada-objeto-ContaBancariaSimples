8a4a25a336ae7587feccbada25b8a8a362e2e00d
Julio-Maciel-Developer <joqbaydigital@gmail.com>
2022-01-27 10:16:53 -0300

    Class criada para add valor para presentes

---
+++ ContaCartaoPresente.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Programacao_orientada_objeto_ContaBancariaSimples
+{
+    public class ContaCartaoPresente : ContaBancaria
+    {
+        private decimal _depositoMensal = 0m;
+
+        public ContaCartaoPresente(string nome, decimal saldoInicialBalanco, decimal depositoMensal = 0) : base(nome, saldoInicialBalanco)
+            => _depositoMensal = depositoMensal;
+
+        public override void RealizarTransacaoDeFimDeMes()
+        {
+            if (_depositoMensal != 0)
+            {
+                FazerDeposito(_depositoMensal, DateTime.Now, "Adicionar depósito mensal");
+            }
+
+    }
+}
