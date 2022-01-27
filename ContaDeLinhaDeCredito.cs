fe5b098fc4224814115e749a25f3eb530dc5bdbc
Julio-Maciel-Developer <joqbaydigital@gmail.com>
2022-01-27 10:18:40 -0300

    Classe criada para caso específico quando precisa-se de crédito

---
+++ ContaDeLinhaDeCredito.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Programacao_orientada_objeto_ContaBancariaSimples
+{
+    public class ContaDeLinhaDeCredito : ContaBancaria
+    {
+        public ContaDeLinhaDeCredito(string nome, decimal saldoInicialBalanco) : base(nome, saldoInicialBalanco)
+        {
+        }
+
+        public override void RealizarTransacaoDeFimDeMes()
+        {
+            // Negar o saldo para obter uma taxa de juros positiva:
+            if (SaldoBalanco < 0)
+            {
+                var juros = -SaldoBalanco * 0.07m;
+                FazerSaque(juros, DateTime.Now, "Cobrar juros mensais");
+            }
+        }
+
+        protected override Transacao? ChecarLimiteDeSaqueParaTerOpcoes(bool issoEOverdrawn) => issoEOverdrawn
+                ? new Transacao(-20, DateTime.Now, "Aplicar taxa ao cheque especial")
+                : default;
+    }
+}
